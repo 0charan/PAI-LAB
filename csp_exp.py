837af70b63cbdeb78d503f98510ba85135ae25e7
0charan <pleelasaicharan@gmail.com>
2026-02-03 11:53:08 +0530

    Add files via upload

---
+++ csp_exp.py
@@ -0,0 +1,72 @@
+class ConstraintSatisfactionProblem:
+    def __init__(self, words):
+        self.words = [w.strip() for w in words]
+        self.variables = sorted(set("".join(self.words)))
+        self.domains = {v: list(range(10)) for v in self.variables}
+        self.constraints = {v: [] for v in self.variables}
+        for v in self.variables:
+            for o in self.variables:
+                if v != o:
+                    self.constraints[v].append(
+                        lambda var, val, ass, o=o: o not in ass or ass[o] != val
+                    )
+        leads = {w[0] for w in self.words}
+        for lead in leads:
+            self.constraints[lead].append(lambda v, val, ass: val != 0)
+    def is_consistent(self, var, val, ass):
+        return all(c(var, val, ass) for c in self.constraints[var])
+    def backtrack(self, ass):
+        if len(ass) == len(self.variables):
+            if self.check(ass):
+                return ass
+            return None
+        var = next(v for v in self.variables if v not in ass)
+        for val in self.domains[var]:
+            if self.is_consistent(var, val, ass):
+                ass[var] = val
+                res = self.backtrack(ass)
+                if res:
+                    return res
+                ass.pop(var)
+        return None
+    def word_value(self, word, a):
+        """Sum of letter values (normal CSP calculation)"""
+        return sum(a[ch] * (10 ** i) for i, ch in enumerate(reversed(word)))
+    def word_product(self, word, a):
+        """Multiply the letter values"""
+        product = 1
+        for ch in word:
+            product *= a[ch]
+        return product
+    def check(self, a):
+        total = sum(self.word_value(w, a) for w in self.words[:-1])
+        return total == self.word_value(self.words[-1], a)
+num_words = int(input("Enter the number of words (including the result): "))
+words = []
+for i in range(num_words):
+    if i < num_words - 1:
+        w = input(f"Enter word {i+1} to sum: ").strip()
+    else:
+        w = input(f"Enter result word: ").strip()
+    words.append(w)
+csp = ConstraintSatisfactionProblem(words)
+solution = csp.backtrack({})
+if solution:
+    print("\nAssigned Values:")
+    for k in sorted(solution):
+        print(f"{k} = {solution[k]}")
+
+    print("\nResult (sum values):")
+    for w in words:
+        print(f"{w} = {csp.word_value(w, solution)}")
+    while True:
+        query = input("\nEnter a word to multiply its letter values (or 'exit' to quit): ").strip()
+        if query.lower() == 'exit':
+            break
+        try:
+            product = csp.word_product(query, solution)
+            print(f"Product of {query}: {product}")
+        except KeyError:
+            print("Word contains letters not in the solution!")
+else:
+    print("\nNo solution found")
